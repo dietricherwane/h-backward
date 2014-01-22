799c88f2a615799f4bb0701c35e3d167a6cc3b50
Mensah Dietrich <erwan@ubuntu.(none)>
2014-01-22 02:19:25 -0800

    changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,8 @@ HubsBackOffice::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = ['.js', '.css', '*.css.erb']
+  #config.assets.compile = false
 
   # Generate digests for assets URLs.
   config.assets.digest = true
