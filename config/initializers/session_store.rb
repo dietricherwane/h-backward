6635038d139bc0891b3875da78a40632babcf96b
Mensah Dietrich <erwan@ubuntu.(none)>
2014-01-22 01:02:12 -0800

    changes

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 # Be sure to restart your server when you modify this file.
 
-#HubsBackOffice::Application.config.session_store :cookie_store, key: '_HubsBackOffice_session', :expire_after => 10.minutes
-HubsBackOffice::Application.config.session_store :active_record_store, { :expire_after => 10.minutes }
+HubsBackOffice::Application.config.session_store :cookie_store, key: '_HubsBackOffice_session', :expire_after => 10.minutes
+#HubsBackOffice::Application.config.session_store :active_record_store, { :expire_after => 10.minutes }
 #ActiveRecord::SessionStore::Session.attr_accessible :data, :session_id
