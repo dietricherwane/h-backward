451b254b798e843e04b62a115bb5e4b64f34949c
nsia <erwan@ubuntu.(none)>
2014-06-20 11:00:59 +0000

    changes

--- app/models/basket.rb
+++ app/models/basket.rb
@@ -2,5 +2,5 @@ class Basket < ActiveRecord::Base
   belongs_to :service
   belongs_to :operation
   belongs_to :currency
-  attr_accessible :number, :fees, :service_id, :payment_status, :paid_transaction_amount, :paid_currency_id, :notified_to_ecommerce, :operation_id, :currency_id, :transaction_id, :transaction_amount, :notified_to_back_office
+  attr_accessible :number, :fees, :rate, :service_id, :payment_status, :paid_transaction_amount, :paid_currency_id, :notified_to_ecommerce, :operation_id, :currency_id, :transaction_id, :transaction_amount, :notified_to_back_office
 end
