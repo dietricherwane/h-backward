68041a024ad70d92a5d1f83f46052e42408d0142
nsia <erwan@ubuntu.(none)>
2014-06-23 11:30:19 +0000

    changes

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,6 +1,4 @@
 class MainController < ApplicationController
-  #@@url = "http://localhost:8080"
-  @@url = "http://41.189.40.193:8080"
   # Only for guard action, we check if service_id exists and initialize a session variable containing transaction_data
   before_action :only => :guard do |s| s.get_service_by_token(params[:currency], params[:service_token], params[:operation_token], params[:order], params[:transaction_amount]) end
   # Only for guard action, we check if the session varable is initialized, if the operation_id is initialized and if transaction_amount is a number
