c6267cfc6638e6e38961971e7af1319a993b3000
NGSER <dietricherwane@yahoo.fr>
2016-06-29 10:42:48 +0000

    changes

--- config/routes.rb
+++ config/routes.rb
@@ -73,6 +73,8 @@ HubsBackOffice::Application.routes.draw do
   get "/UBA" => "uba#index"
   post "/UBA/validate" => "uba#validate_transaction"
   post "/UBA/transaction_acknowledgement" => "uba#transaction_acknowledgement"
+  get "/UBA/transaction_acknowledgement" => "uba#transaction_acknowledgement"
+
 
   get "novapay/:service_id/:operation_id/:basket_number/:transaction_amount" => "novapays#guard", :constraints => {:transaction_amount => /(\d+(.\d+)?)/}
   get "NovaPay" => "novapays#index"
