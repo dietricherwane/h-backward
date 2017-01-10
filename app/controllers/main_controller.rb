051f2c1ad8e2f93440e72cd58891db860f0eafd4
Abou Bakr GBANE <abou.gbane@ngser.com>
2017-01-10 15:59:06 +0000

    Refactor business layer and MTN MoMo controller

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -2,7 +2,7 @@ class MainController < ApplicationController
   # Only for guard action, we check if service_id exists and initialize a session variable containing transaction_data
   before_action :only => :guard do |s| s.get_service_by_token(params[:currency], params[:service_token], params[:operation_token], params[:order], params[:transaction_amount], params[:id], params[:paymoney_account_number], params[:paymoney_password]) end
   # Only for guard action, we check if the session varable is initialized, if the operation_id is initialized and if transaction_amount is a number
-  before_action :only => [:guard, :index] do |o| o.filter_connections end
+  before_action :filter_connections, only: [:guard, :index]
   ##before_action :only => :guard do |r| r.authenticate_incoming_request(params[:operation_id], params[:basket_number], params[:transaction_amount]) end
   # Vérifie que le panier n'a pas déjà été payé via paypal
   before_action :only => :guard do |s| s.basket_already_paid?(session[:basket]["basket_number"]) end
@@ -16,11 +16,7 @@ class MainController < ApplicationController
   layout :select_layout
 
   def select_layout
-    if session[:service].authentication_token == '57813dc7992fbdc721ca5f6b0d02d559'
-      return "guce"
-    else
-      return "main"
-    end
+    session[:service].authentication_token == '57813dc7992fbdc721ca5f6b0d02d559' ? 'guce' : 'main'
   end
 
   # Reçoit les requêtes venant des différents services
@@ -32,6 +28,9 @@ class MainController < ApplicationController
   def index
     @countries = Country.where("published IS TRUE").order("name ASC")
     @international = Country.find_by_name("Banques")
-    @available_wallets = session[:service].available_wallets.where(published: true, wallet_id: session[:service].wallets.where(country_id: @international.id).map{|w| w.id})
+    @available_wallets = session[:service].available_wallets.where(
+      published: true, 
+      wallet_id: session[:service].wallets.where(country_id: @international.id).map{|w| w.id}
+    )
   end
 end
