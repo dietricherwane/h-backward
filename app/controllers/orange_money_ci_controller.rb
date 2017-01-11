d636a93c9f6bf2859fbce49b5f54ac6ba93c5409
Abou Bakr GBANE <abou.gbane@ngser.com>
2017-01-11 18:12:38 +0000

    Refactor Orange Money controller

--- app/controllers/orange_money_ci_controller.rb
+++ app/controllers/orange_money_ci_controller.rb
@@ -1,4 +1,5 @@
 class OrangeMoneyCiController < ApplicationController
+  @@wallet_name = 'orange_money_ci'
   ##before_action :only => :guard do |o| o.filter_connections end
   before_action :session_exists?, :except => [:ipn, :transaction_acknowledgement, :initialize_session, :session_initialized, :payment_result_listener, :generic_ipn_notification, :cashout]
   # Si l'utilisateur ne s'est pas connecté en passant par main#guard, on le rejette
@@ -12,11 +13,7 @@ class OrangeMoneyCiController < ApplicationController
   layout :select_layout
 
   def select_layout
-    if session[:service].authentication_token == '57813dc7992fbdc721ca5f6b0d02d559'
-      return "guce"
-    else
-      return "orange_money_ci"
-    end
+    session[:service].authentication_token == '57813dc7992fbdc721ca5f6b0d02d559' ? 'guce' : 'orange_money_ci'
   end
 
   # Reçoit les requêtes venant des différents services
@@ -57,13 +54,23 @@ class OrangeMoneyCiController < ApplicationController
         paymoney_password: session[:paymoney_password]
       )
     else
-      @basket.first.update_attributes(:transaction_amount => session[:trs_amount].to_f.ceil, :original_transaction_amount => session[:trs_amount], :currency_id => session[:currency].id, :paid_transaction_amount => @transaction_amount, :paid_currency_id => @wallet_currency.id, :fees => @shipping, :rate => @rate, :login_id => session[:login_id], paymoney_account_number: session[:paymoney_account_number], paymoney_account_token: session[:paymoney_account_token], paymoney_password: session[:paymoney_password])
+      @basket.first.update_attributes(
+        transaction_amount: session[:trs_amount].to_f.ceil, 
+        original_transaction_amount: session[:trs_amount], 
+        currency_id: session[:currency].id, 
+        paid_transaction_amount: @transaction_amount, 
+        paid_currency_id: @wallet_currency.id, 
+        fees: @shipping, 
+        rate: @rate, 
+        login_id: session[:login_id], 
+        paymoney_account_number: session[:paymoney_account_number], 
+        paymoney_account_token: session[:paymoney_account_token], 
+        paymoney_password: session[:paymoney_password]
+      )
     end
 
     initialize_session
-    unless session_initialized
-      redirect_to error_page_path
-    end
+    redirect_to error_page_path unless session_initialized
   end
 
   def payment_result_listener
@@ -79,16 +86,20 @@ class OrangeMoneyCiController < ApplicationController
       if valid_transaction
         @basket = OrangeMoneyCiBasket.find_by_transaction_id(@transaction_id)
         if @basket
-
           # Use Orange Money authentication_token
           update_wallet_used(@basket, "b005fd07f0")
 
           if (@basket.paid_transaction_amount + @basket.fees) == @transaction_amount.to_f
-
             # Conversion du montant débité par le wallet et des frais en euro avant envoi pour notification au back office du hub
             @rate = get_change_rate("XAF", "EUR")
 
-            @basket.update_attributes(payment_status: true, ompay_token: @token, ompay_clientid: @clientid, ompay_payid: @payid, compensation_rate: @rate)
+            @basket.update_attributes(
+              payment_status: true, 
+              ompay_token: @token, 
+              ompay_clientid: @clientid, 
+              ompay_payid: @payid, 
+              compensation_rate: @rate
+            )
 
             @amount_for_compensation = ((@basket.paid_transaction_amount + @basket.fees) * @rate).round(2)
             @fees_for_compensation = (@basket.fees * @rate).round(2)
@@ -110,24 +121,21 @@ class OrangeMoneyCiController < ApplicationController
             if (@basket.operation.authentication_token rescue nil) == "b6dff4ae-05c1-4050-a976-0db6e358f22b"
               redirect_to "http://ekioskmobile.net/retourabonnement.php?transaction_id=#{@basket.transaction_id}&order_id=#{@basket.number}&status_id=1&wallet=orange_money_ci&transaction_amount=#{@basket.original_transaction_amount}&currency=#{@basket.currency.code}&paid_transaction_amount=#{@basket.paid_transaction_amount}&paid_currency=#{Currency.find_by_id(@basket.paid_currency_id).code}&change_rate=#{@basket.rate}&id=#{@basket.login_id}".
             else
-
               # Cashin mobile money
               if ['3d20d7af-2ecb-4681-8e4f-a585d7700ee4', '0acae92d-d63c-41d7-b385-d797b95e98dc', '7489bd19-6ef8-4748-8218-ac9201512345', 'ebb1f4f3-116b-417e-8348-5964771d0123', 's8g56da9-63f1-486e-9b0c-eceb0aab6d6c'].include?(@basket.operation.authentication_token)
                 operation_token = '6fb8a45e'
                 mobile_money_token = '064b6e92'
                 reload_request = "#{ENV['gateway_wallet_url']}/api/86d138798bc43ed59e5207c664/mobile_money/cashin/Orange/#{operation_token}/#{mobile_money_token}/#{@basket.paymoney_account_number}/#{@basket.transaction_id}/#{@basket.original_transaction_amount}/0"
                 reload_response = (RestClient.get(reload_request) rescue "")
-                if reload_response.include?('|')
-                  @status_id = '5'
-                end
+                @status_id = '5' if reload_response.include?('|')
                 @basket.update_attributes(paymoney_reload_request: reload_request, paymoney_reload_response: reload_response, paymoney_transaction_id: ((reload_response.blank? || reload_response.include?('|')) ? nil : reload_response))
               end
               # Cashin mobile money
-
-              redirect_to "#{@basket.service.url_on_success}?transaction_id=#{@basket.transaction_id}&order_id=#{@basket.number}&status_id=#{@status_id}&wallet=orange_money_ci&transaction_amount=#{@basket.original_transaction_amount}&currency=#{@basket.currency.code}&paid_transaction_amount=#{@basket.paid_transaction_amount}&paid_currency=#{Currency.find_by_id(@basket.paid_currency_id).code}&change_rate=#{@basket.rate}&id=#{@basket.login_id}"
+              # redirect_to "#{@basket.service.url_on_success}?transaction_id=#{@basket.transaction_id}&order_id=#{@basket.number}&status_id=#{@status_id}&wallet=orange_money_ci&transaction_amount=#{@basket.original_transaction_amount}&currency=#{@basket.currency.code}&paid_transaction_amount=#{@basket.paid_transaction_amount}&paid_currency=#{Currency.find_by_id(@basket.paid_currency_id).code}&change_rate=#{@basket.rate}&id=#{@basket.login_id}"
+              redirect_to notification_url(@basket, true, @@wallet_name)
             end
           else
-            @basket.update_attributes(:conflictual_transaction_amount => @transaction_amount.to_f, :conflictual_currency => "XAF")
+            @basket.update_attributes(conflictual_transaction_amount: @transaction_amount.to_f, conflictual_currency: "XAF")
 
             # Update in available_wallet the number of failed_transactions
             update_number_of_failed_transactions
@@ -135,7 +143,8 @@ class OrangeMoneyCiController < ApplicationController
             if (@basket.operation.authentication_token rescue nil) == "b6dff4ae-05c1-4050-a976-0db6e358f22b"
               redirect_to "http://ekioskmobile.net/retourabonnement.php?transaction_id=#{@basket.transaction_id}&order_id=#{@basket.number}&status_id=0&wallet=orange_money_ci&transaction_amount=#{@basket.original_transaction_amount}&currency=#{@basket.currency.code}&paid_transaction_amount=&paid_currency=&change_rate=#{@basket.rate}&conflictual_transaction_amount=#{@basket.conflictual_transaction_amount}&conflictual_currency=#{@basket.conflictual_currency}&id=#{@basket.login_id}"
             else
-              redirect_to "#{@basket.service.url_on_success}?transaction_id=#{@basket.transaction_id}&order_id=#{@basket.number}&status_id=0&wallet=orange_money_ci&transaction_amount=#{@basket.original_transaction_amount}&currency=#{@basket.currency.code}&paid_transaction_amount=&paid_currency=&change_rate=#{@basket.rate}&conflictual_transaction_amount=#{@basket.conflictual_transaction_amount}&conflictual_currency=#{@basket.conflictual_currency}&id=#{@basket.login_id}"
+              # redirect_to "#{@basket.service.url_on_success}?transaction_id=#{@basket.transaction_id}&order_id=#{@basket.number}&status_id=0&wallet=orange_money_ci&transaction_amount=#{@basket.original_transaction_amount}&currency=#{@basket.currency.code}&paid_transaction_amount=&paid_currency=&change_rate=#{@basket.rate}&conflictual_transaction_amount=#{@basket.conflictual_transaction_amount}&conflictual_currency=#{@basket.conflictual_currency}&id=#{@basket.login_id}"
+              redirect_to notification_url(@basket, true, @@wallet_name)
             end
           end
         else
@@ -144,7 +153,8 @@ class OrangeMoneyCiController < ApplicationController
       else
         @basket = OrangeMoneyCiBasket.find_by_transaction_id(@transaction_id) rescue nil
         @basket.update_attributes(payment_status: true, ompay_token: @token, ompay_clientid: @clientid) rescue nil
-        redirect_to "#{@basket.service.url_on_success}?transaction_id=#{@basket.transaction_id}&order_id=#{@basket.number}&status_id=0&wallet=orange_money_ci&transaction_amount=#{@basket.original_transaction_amount}&currency=#{@basket.currency.code}&paid_transaction_amount=&paid_currency=&change_rate=#{@basket.rate}&conflictual_transaction_amount=#{@basket.conflictual_transaction_amount}&conflictual_currency=#{@basket.conflictual_currency}&id=#{@basket.login_id}"
+        # redirect_to "#{@basket.service.url_on_success}?transaction_id=#{@basket.transaction_id}&order_id=#{@basket.number}&status_id=0&wallet=orange_money_ci&transaction_amount=#{@basket.original_transaction_amount}&currency=#{@basket.currency.code}&paid_transaction_amount=&paid_currency=&change_rate=#{@basket.rate}&conflictual_transaction_amount=#{@basket.conflictual_transaction_amount}&conflictual_currency=#{@basket.conflictual_currency}&id=#{@basket.login_id}"
+        redirect_to notification_url(@basket, true, @@wallet_name)
         #render text: "La transaction n'existe pas - O"#redirect_to error_page_path
       end
     else
@@ -153,18 +163,12 @@ class OrangeMoneyCiController < ApplicationController
   end
 
   def valid_result_parameters
-    if !@transaction_id.blank? && !@token.blank? && !@clientid.blank? && !@transaction_amount.blank? && !@status.blank?
-      return true
-    else
-      return false
-    end
+    @transaction_id && @token && @clientid && @transaction_amount && @status
   end
 
   def cashout
     @cashout_account_number = params[:cashout_account_number]
-
     @transaction_id = params[:purchaseref]
-
     @basket = OrangeMoneyCiBasket.find_by_transaction_id(@transaction_id)
 
     if @cashout_account_number.blank?
@@ -176,7 +180,7 @@ class OrangeMoneyCiController < ApplicationController
 
       render :index
     else
-      if !@basket.blank?
+      if @basket
         # Cashout mobile money
         operation_token = '3e7573a1'
         mobile_money_token = '064b6e92'
@@ -188,18 +192,34 @@ class OrangeMoneyCiController < ApplicationController
           @status_id = '0'
           # Update in available_wallet the number of failed_transactions
           update_number_of_failed_transactions
-          @basket.update_attributes(payment_status: false, cashout: true, cashout_completed: false, paymoney_reload_request: unload_request, paymoney_reload_response: unload_response, paymoney_transaction_id: unload_response, cashout_account_number: @cashout_account_number)
+          @basket.update_attributes(
+            payment_status: false, 
+            cashout: true, 
+            cashout_completed: false, 
+            paymoney_reload_request: unload_request, 
+            paymoney_reload_response: unload_response, 
+            paymoney_transaction_id: unload_response, 
+            cashout_account_number: @cashout_account_number
+          )
         else
           @status_id = '5'
           # Update in available_wallet the number of successful_transactions
           #update_number_of_succeed_transactions
-          @basket.update_attributes(payment_status: true, cashout: true, cashout_completed: true, paymoney_reload_request: unload_request, paymoney_reload_response: unload_response, cashout_account_number: @cashout_account_number)
+          @basket.update_attributes(
+            payment_status: true, 
+            cashout: true, 
+            cashout_completed: true, 
+            paymoney_reload_request: unload_request, 
+            paymoney_reload_response: unload_response, 
+            cashout_account_number: @cashout_account_number
+          )
         end
 
         # Saves the transaction on the front office
         save_cashout_log
 
-        redirect_to "#{@basket.service.url_on_success}?transaction_id=#{@basket.transaction_id}&order_id=#{@basket.number}&status_id=#{@status_id}&wallet=orange_money_ci&transaction_amount=#{@basket.original_transaction_amount}&currency=#{@basket.currency.code}&paid_transaction_amount=#{@basket.paid_transaction_amount}&paid_currency=#{Currency.find_by_id(@basket.paid_currency_id).code}&change_rate=#{@basket.rate}"
+        # redirect_to "#{@basket.service.url_on_success}?transaction_id=#{@basket.transaction_id}&order_id=#{@basket.number}&status_id=#{@status_id}&wallet=orange_money_ci&transaction_amount=#{@basket.original_transaction_amount}&currency=#{@basket.currency.code}&paid_transaction_amount=#{@basket.paid_transaction_amount}&paid_currency=#{Currency.find_by_id(@basket.paid_currency_id).code}&change_rate=#{@basket.rate}"
+        redirect_to notification_url(@basket, true, @@wallet_name)
         # Cashout mobile money
       else
         redirect_to error_page_path
@@ -212,7 +232,7 @@ class OrangeMoneyCiController < ApplicationController
       fee_type = FeeType.find_by_token('0175ad')
       @shipping = 0
 
-      if !fee_type.blank?
+      if fee_type
 	      @shipping = ((fee_type.fees.where("min_value <= #{session[:trs_amount].to_f} AND max_value >= #{session[:trs_amount].to_f}").first.fee_value) * @rate).ceil.round(2)
 	    end
 	  end
@@ -223,18 +243,18 @@ class OrangeMoneyCiController < ApplicationController
     log_request = "#{ENV['front_office_url']}/api/856332ed59e5207c68e864564/cashout/log/orange_money_ci?transaction_id=#{@basket.transaction_id}&order_id=#{@basket.number}&status_id=#{@status_id}&transaction_amount=#{@basket.original_transaction_amount}&currency=#{@basket.currency.code}&paid_transaction_amount=#{@basket.paid_transaction_amount}&paid_currency=#{Currency.find_by_id(@basket.paid_currency_id).code}&change_rate=#{@basket.rate}&id=#{@basket.login_id}&cashout_account_number=#{@cashout_account_number}&fee=#{@basket.fees}"
     log_response = (RestClient.get(log_request) rescue "")
 
-    @basket.update_attributes(cashout_notified_to_front_office: (log_response == '1' ? true : false), cashout_notification_request: log_request, cashout_notification_response: log_response)
+    @basket.update_attributes(
+      cashout_notified_to_front_office: (log_response == '1' ? true : false), 
+      cashout_notification_request: log_request, 
+      cashout_notification_response: log_response
+    )
   end
 
   def valid_transaction
     request = Typhoeus::Request.new(ENV['orange_money_verify_url'], body: "merchantid=#{ENV['orange_money_merchant_id']}&token=#{@token}", headers: {:'Content-Type'=> "application/x-www-form-urlencoded"}, followlocation: true, method: :post)
 
     request.on_complete do |response|
-      if response.success?
-        @result = response.body.strip rescue nil
-      else
-        @result = nil
-      end
+      @result = response.success? ? response.body.strip rescue nil : nil
     end
 
     request.run
@@ -268,7 +288,7 @@ class OrangeMoneyCiController < ApplicationController
   end
 
   def session_initialized
-    (@session_id != "access denied" && @session_id != nil && @session_id.length > 30) ? true : false
+    @session_id != "access denied" && @session_id != nil && @session_id.length > 30
   end
 
   def notify_to_back_office(basket, url)
@@ -283,9 +303,7 @@ class OrangeMoneyCiController < ApplicationController
     "
     run_typhoeus_request(@request, @internal_com_request)
 
-    if @status.to_s.strip == "1"
-      basket.update_attributes(:notified_to_back_office => true)
-    end
+    basket.update_attributes(notified_to_back_office: true) if @status.to_s.strip == "1"
   end
 
   # Returns 0 or 1 depending on the status of the transaction
@@ -299,8 +317,6 @@ class OrangeMoneyCiController < ApplicationController
     # wallet=05ccd7ba3d
     @request.run
     @response = @request.response
-    if @response.code.to_s == "200"
-      basket.update_attributes(:notified_to_ecommerce => true)
-    end
+    basket.update_attributes(notified_to_ecommerce: true) if @response.code.to_s == "200"
   end
 end
