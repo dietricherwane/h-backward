fb1c664f1704c98e154b258334e696e64adbf673
Abou Bakr GBANE <abou.gbane@ngser.com>
2017-01-12 11:44:08 +0000

    Refactor Qash Controller

--- app/controllers/qash_baskets_controller.rb
+++ app/controllers/qash_baskets_controller.rb
@@ -1,12 +1,13 @@
 class QashBasketsController < ApplicationController
+  @@wallet_name = 'qash_services'
 
   ##before_action :only => :guard do |o| o.filter_connections end
-  before_action :session_exists?, :except => [:ipn, :transaction_acknowledgement, :payment_result_listener, :generic_ipn_notification, :cashout]
+  before_action :session_exists?, :only => [:index, :guard, :set_cashout_fee, :valid_result_parameters, :valid_transaction, :notify_to_back_office, :save_cashout_log]
   # Si l'utilisateur ne s'est pas connecté en passant par main#guard, on le rejette
-  before_action :except => [:ipn, :transaction_acknowledgement, :payment_result_listener, :generic_ipn_notification, :cashout] do |s| s.session_authenticated? end
+  before_action :session_authenticated?, :only => [:index, :guard, :set_cashout_fee, :valid_result_parameters, :valid_transaction, :notify_to_back_office, :save_cashout_log]
 
   # Set transaction amount for GUCE requests
-  before_action :only => :index do |o| o.guce_request? end
+  before_action :set_guce_transaction_amount, :only => :index
 
   layout "qash"
 
@@ -43,7 +44,19 @@ class QashBasketsController < ApplicationController
         paymoney_password: session[:paymoney_password]
       )
     else
-      @basket.first.update_attributes(:transaction_amount => session[:trs_amount].to_f.ceil, :original_transaction_amount => session[:trs_amount], :currency_id => session[:currency].id, :paid_transaction_amount => @transaction_amount, :paid_currency_id => @wallet_currency.id, :fees => @shipping, :rate => @rate, :login_id => session[:login_id], paymoney_account_number: session[:paymoney_account_number], paymoney_account_token: session[:paymoney_account_token], paymoney_password: session[:paymoney_password])
+      @basket.first.update_attributes(
+        transaction_amount: session[:trs_amount].to_f.ceil, 
+        original_transaction_amount: session[:trs_amount], 
+        currency_id: session[:currency].id, 
+        paid_transaction_amount: @transaction_amount, 
+        paid_currency_id: @wallet_currency.id, 
+        fees: @shipping, 
+        rate: @rate, 
+        login_id: session[:login_id], 
+        paymoney_account_number: session[:paymoney_account_number], 
+        paymoney_account_token: session[:paymoney_account_token], 
+        paymoney_password: session[:paymoney_password]
+      )
     end
   end
 
@@ -60,17 +73,19 @@ class QashBasketsController < ApplicationController
       if valid_transaction
         @basket = QashBasket.find_by_transaction_id(@transaction_id)
         if @basket
-
           # Use Qash authentication_token
           update_wallet_used(@basket, "936166e255")
 
           @devise.to_s.upcase.strip == "CFA" ? (@devise = "XAF") : (@devise = nil)
           if (@basket.paid_transaction_amount + @basket.fees) == @transaction_amount.to_f  && (Currency.find_by_code(@basket.paid_currency_id).code.upcase rescue "") == @devise.upcase
-
             # Conversion du montant débité par le wallet et des frais en euro avant envoi pour notification au back office du hub
             @rate = get_change_rate(@devise, "EUR")
 
-            @basket.update_attributes(payment_status: true, qash_transaction_id: @qash_transaction_id, compensation_rate: @rate)
+            @basket.update_attributes(
+              payment_status: true, 
+              qash_transaction_id: @qash_transaction_id, 
+              compensation_rate: @rate
+            )
 
             @amount_for_compensation = ((@basket.paid_transaction_amount + @basket.fees) * @rate).round(2)
             @fees_for_compensation = (@basket.fees * @rate).round(2)
@@ -94,22 +109,28 @@ class QashBasketsController < ApplicationController
               mobile_money_token = '02523ec1'
               reload_request = "#{ENV['gateway_wallet_url']}/api/86d138798bc43ed59e5207c664/mobile_money/cashin/QS/#{operation_token}/#{mobile_money_token}/#{@basket.paymoney_account_number}/#{@basket.transaction_id}/#{@basket.original_transaction_amount}/0"
               reload_response = (RestClient.get(reload_request) rescue "")
-              if reload_response.include?('|')
-                @status_id = '5'
-              end
-              @basket.update_attributes(paymoney_reload_request: reload_request, paymoney_reload_response: reload_response, paymoney_transaction_id: ((reload_response.blank? || reload_response.include?('|')) ? nil : reload_response))
+              
+              @status_id = '5' if reload_response.include?('|')
+
+              @basket.update_attributes(
+                paymoney_reload_request: reload_request, 
+                paymoney_reload_response: reload_response, 
+                paymoney_transaction_id: ((reload_response.blank? || reload_response.include?('|')) ? nil : reload_response)
+              )
             end
             # Cashin mobile money
 
             # Redirection vers le site marchand
-            redirect_to "#{@basket.service.url_on_success}?transaction_id=#{@basket.transaction_id}&order_id=#{@basket.number}&status_id=#{@status_id}&wallet=qash_services&transaction_amount=#{@basket.original_transaction_amount}&currency=#{@basket.currency.code}&paid_transaction_amount=#{@basket.paid_transaction_amount}&paid_currency=#{Currency.find_by_id(@basket.paid_currency_id).code}&change_rate=#{@basket.rate}&id=#{@basket.login_id}"
+            # redirect_to "#{@basket.service.url_on_success}?transaction_id=#{@basket.transaction_id}&order_id=#{@basket.number}&status_id=#{@status_id}&wallet=qash_services&transaction_amount=#{@basket.original_transaction_amount}&currency=#{@basket.currency.code}&paid_transaction_amount=#{@basket.paid_transaction_amount}&paid_currency=#{Currency.find_by_id(@basket.paid_currency_id).code}&change_rate=#{@basket.rate}&id=#{@basket.login_id}"
+            redirect_to notification_url(@basket, true, @@wallet_name)
           else
             @basket.update_attributes(:conflictual_transaction_amount => @transaction_amount.to_f, :conflictual_currency => @devise.to_s[0..2].upcase)
 
             # Update in available_wallet the number of failed_transactions
             update_number_of_failed_transactions
 
-            redirect_to "#{@basket.service.url_on_success}?transaction_id=#{@basket.transaction_id}&order_id=#{@basket.number}&status_id=0&wallet=qash_services&transaction_amount=#{@basket.original_transaction_amount}&currency=#{@basket.currency.code}&paid_transaction_amount=&paid_currency=&change_rate=#{@basket.rate}&conflictual_transaction_amount=#{@basket.conflictual_transaction_amount}&conflictual_currency=#{@basket.conflictual_currency}&id=#{@basket.login_id}"
+            # redirect_to "#{@basket.service.url_on_success}?transaction_id=#{@basket.transaction_id}&order_id=#{@basket.number}&status_id=0&wallet=qash_services&transaction_amount=#{@basket.original_transaction_amount}&currency=#{@basket.currency.code}&paid_transaction_amount=&paid_currency=&change_rate=#{@basket.rate}&conflictual_transaction_amount=#{@basket.conflictual_transaction_amount}&conflictual_currency=#{@basket.conflictual_currency}&id=#{@basket.login_id}"
+            redirect_to notification_url(@basket, true, @@wallet_name)
           end
         else
           redirect_to error_page_path
@@ -123,27 +144,19 @@ class QashBasketsController < ApplicationController
   end
 
   def valid_result_parameters
-    if !@qash_transaction_id.blank? && !@transaction_id.blank? && !@transaction_amount.blank? && !@devise.blank? && !@status.blank? && !@name.blank? && !@merchant_id.blank?
-      return true
-    else
-      return false
-    end
+    @qash_transaction_id && @transaction_id && @transaction_amount && @devise && @status && @name && @merchant_id
   end
 
   def valid_transaction
     request = Typhoeus::Request.new("#{ENV['qash_verify_url']}?TXN_ID=#{@qash_transaction_id}&ID_OPERATION=#{@transaction_id}&REF_COMMERCE=#{@merchant_id}&MONTANT=#{@transaction_amount}&DEVISE=#{@devise}&ETAT=#{@status}&NOM_PREN=#{@name}", followlocation: true, method: :get)
 
     request.on_complete do |response|
-      if response.success?
-        @result = response.body.strip rescue nil
-      else
-        @result = nil
-      end
+      @result = response.success? ? (response.body.strip rescue nil) : nil
     end
 
     request.run
 
-    @result == "VERIFIED" ? true : false
+    @result == "VERIFIED"
   end
 
   def ipn
@@ -159,9 +172,7 @@ class QashBasketsController < ApplicationController
     "
     run_typhoeus_request(@request, @internal_com_request)
 
-    if @status.to_s.strip == "1"
-      basket.update_attributes(:notified_to_back_office => true)
-    end
+    basket.update_attributes(notified_to_back_office: true) if @status.to_s.strip == "1"
   end
 
   # Returns 0 or 1 depending on the status of the transaction
@@ -171,13 +182,16 @@ class QashBasketsController < ApplicationController
 
   def generic_ipn_notification(basket)
     @service = Service.find_by_id(basket.service_id)
-    @request = Typhoeus::Request.new("#{@service.url_to_ipn}?transaction_id=#{@basket.transaction_id}&order_id=#{@basket.number}&status_id=1&wallet=qash_services&transaction_amount=#{@basket.original_transaction_amount}&currency=#{@basket.currency.code}&paid_transaction_amount=#{@basket.paid_transaction_amount}&paid_currency=#{Currency.find_by_id(@basket.paid_currency_id).code}&change_rate=#{@basket.rate}&id=#{@basket.login_id}", followlocation: true, method: :post)
+    @request = Typhoeus::Request.new(
+      "#{@service.url_to_ipn}" + "?" + notification_parameters(basket, @@wallet_name), 
+      followlocation: true, 
+      method: :post
+    )
     # wallet=05ccd7ba3d
     @request.run
     @response = @request.response
-    if @response.code.to_s == "200"
-      basket.update_attributes(:notified_to_ecommerce => true)
-    end
+    
+    basket.update_attributes(notified_to_ecommerce: true) if @response.code.to_s == "200"
   end
 
   def cashout
@@ -196,7 +210,7 @@ class QashBasketsController < ApplicationController
 
       render :index
     else
-      if !@basket.blank?
+      if @basket
         # Cashout mobile money
         operation_token = '40b29ddf'
         mobile_money_token = '02523ec1'
@@ -208,18 +222,34 @@ class QashBasketsController < ApplicationController
           @status_id = '0'
           # Update in available_wallet the number of failed_transactions
           update_number_of_failed_transactions
-          @basket.update_attributes(payment_status: false, cashout: true, cashout_completed: false, paymoney_reload_request: unload_request, paymoney_reload_response: unload_response, paymoney_transaction_id: unload_response, cashout_account_number: @cashout_account_number)
+          @basket.update_attributes(
+            payment_status: false, 
+            cashout: true, 
+            cashout_completed: false, 
+            paymoney_reload_request: unload_request, 
+            paymoney_reload_response: unload_response, 
+            paymoney_transaction_id: unload_response, 
+            cashout_account_number: @cashout_account_number
+          )
         else
           @status_id = '5'
           # Update in available_wallet the number of successful_transactions
           #update_number_of_succeed_transactions
-          @basket.update_attributes(payment_status: true, cashout: true, cashout_completed: true, paymoney_reload_request: unload_request, paymoney_reload_response: unload_response, cashout_account_number: @cashout_account_number)
+          @basket.update_attributes(
+            payment_status: true, 
+            cashout: true, 
+            cashout_completed: true, 
+            paymoney_reload_request: unload_request, 
+            paymoney_reload_response: unload_response, 
+            cashout_account_number: @cashout_account_number
+          )
         end
 
         # Saves the transaction on the front office
         save_cashout_log
 
-        redirect_to "#{@basket.service.url_on_success}?transaction_id=#{@basket.transaction_id}&order_id=#{@basket.number}&status_id=#{@status_id}&wallet=qash_services&transaction_amount=#{@basket.original_transaction_amount}&currency=#{@basket.currency.code}&paid_transaction_amount=#{@basket.paid_transaction_amount}&paid_currency=#{Currency.find_by_id(@basket.paid_currency_id).code}&change_rate=#{@basket.rate}"
+        # redirect_to "#{@basket.service.url_on_success}?transaction_id=#{@basket.transaction_id}&order_id=#{@basket.number}&status_id=#{@status_id}&wallet=qash_services&transaction_amount=#{@basket.original_transaction_amount}&currency=#{@basket.currency.code}&paid_transaction_amount=#{@basket.paid_transaction_amount}&paid_currency=#{Currency.find_by_id(@basket.paid_currency_id).code}&change_rate=#{@basket.rate}"
+        redirect_to notification_url(@basket, successfull, @@wallet_name)
         # Cashout mobile money
       else
         redirect_to error_page_path
@@ -232,7 +262,7 @@ class QashBasketsController < ApplicationController
       fee_type = FeeType.find_by_token('0175ad')
       @shipping = 0
 
-      if !fee_type.blank?
+      if fee_type
 	      @shipping = ((fee_type.fees.where("min_value <= #{session[:trs_amount].to_f} AND max_value >= #{session[:trs_amount].to_f}").first.fee_value) * @rate).ceil.round(2)
 	    end
 	  end
@@ -243,7 +273,11 @@ class QashBasketsController < ApplicationController
     log_request = "#{ENV['front_office_url']}/api/856332ed59e5207c68e864564/cashout/log/qash?transaction_id=#{@basket.transaction_id}&order_id=#{@basket.number}&status_id=#{@status_id}&transaction_amount=#{@basket.original_transaction_amount}&currency=#{@basket.currency.code}&paid_transaction_amount=#{@basket.paid_transaction_amount}&paid_currency=#{Currency.find_by_id(@basket.paid_currency_id).code}&change_rate=#{@basket.rate}&id=#{@basket.login_id}&cashout_account_number=#{@cashout_account_number}&fee=#{@basket.fees}"
     log_response = (RestClient.get(log_request) rescue "")
 
-    @basket.update_attributes(cashout_notified_to_front_office: (log_response == '1' ? true : false), cashout_notification_request: log_request, cashout_notification_response: log_response)
+    @basket.update_attributes(
+      cashout_notified_to_front_office: (log_response == '1' ? true : false), 
+      cashout_notification_request: log_request, 
+      cashout_notification_response: log_response
+    )
   end
 
 end
