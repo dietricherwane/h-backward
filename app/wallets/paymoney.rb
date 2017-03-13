376e9d7abe4f0d92a508cd1b7b57df97925101d6
Abou Bakr G <aboubakr.gbane@gmail.com>
2017-03-13 17:15:34 +0000

    Redefines all the wallet classes as static

--- app/wallets/paymoney.rb
+++ app/wallets/paymoney.rb
@@ -1,8 +1,7 @@
 module Wallets
   class Paymoney
-    include HTTParty
 
-    # base_uri ENV['paymoney_wallet_url']
+    BASE_URI = ENV['paymoney_wallet_url']
 
     # {
     #   service_profile_token: ,
@@ -14,38 +13,31 @@ module Wallets
     #   password:
     # }
 
-    def initialize(transaction_details)
-      @transaction_details = transaction_details
-    end
+    class << self
+      def unload(transaction_details = {}) # RequestPayment
+        uri = "/PAYMONEY_WALLET/rest/operation_ecommerce/#{transaction_details[:service_profile_token]}/#{transaction_details[:paymoney_token]}/#{transaction_details[:token]}/#{transaction_details[:transaction_amount]}/#{transaction_details[:fees]}/0/#{transaction_details[:transaction_id]}/#{transaction_details[:password]}"
+        HTTParty.get(BASE_URI + uri)
+      end
 
-    def unload # RequestPayment
-      uri = "/PAYMONEY_WALLET/rest/operation_ecommerce/#{@transaction_details[:service_profile_token]}/#{@transaction_details[:paymoney_token]}/#{@transaction_details[:token]}/#{@transaction_details[:transaction_amount]}/#{@transaction_details[:fees]}/0/#{@transaction_details[:transaction_id]}/#{@transaction_details[:password]}"
-      # HTTParty.post()
-      self.class.get(ENV['paymoney_wallet_url'] + uri)
-    end
+      def reload(transaction_details = {}) # DepositRequest
+        uri = "/PAYMONEY-NGSER/rest/OperationService/CreditOperation/1/#{transaction_details[:account]}/#{transaction_details[:transaction_amount].to_i.abs}"
+        HTTParty.get(BASE_URI + uri)
+      end
 
-    def reload # DepositRequest
-      uri = "/PAYMONEY-NGSER/rest/OperationService/CreditOperation/1/#{@transaction_details[:account]}/#{@transaction_details[:transaction_amount].to_i.abs}"
-      # HTTParty.post()
-      self.class.get(ENV['paymoney_wallet_url'] + uri)
-    end
+      def get_account_token(transaction_details = {})
+        uri = "/PAYMONEY_WALLET/rest/check2_compte/#{transaction_details[:account_number]}"
+        HTTParty.get(BASE_URI + uri)
+      end
 
-    def get_account_token
-      uri = "/PAYMONEY_WALLET/rest/check2_compte/#{@transaction_details[:account_number]}"
-      # HTTParty.post()
-      self.class.get(ENV['paymoney_wallet_url'] + uri)
-    end
+      def change_status(transaction_details = {})
+        uri = "/GATEWAY/rest/ES/ChangeStatus/#{@pin}"
+        HTTParty.get(BASE_URI + uri)
+      end
 
-    def change_status
-      uri = "/GATEWAY/rest/ES/ChangeStatus/#{@pin}"
-      # HTTParty.post()
-      self.class.get(ENV['paymoney_wallet_url'] + uri)
-    end
-
-    def verify_pin
-      uri = "/GATEWAY/rest/ES/VerifyPin/#{@pin}"
-      # HTTParty.post()
-      self.class.get(ENV['paymoney_wallet_url'] + uri)
+      def verify_pin(transaction_details = {})
+        uri = "/GATEWAY/rest/ES/VerifyPin/#{@pin}"
+        HTTParty.get(BASE_URI + uri)
+      end
     end
   end
 end
---
+++ app/wallets/paypal.rb
@@ -0,0 +1,15 @@
+module Wallets
+  class Paypal
+
+    class << self
+      def unload # RequestPayment
+      end
+
+      def reload # DepositRequest
+      end
+
+      def build_request_payload(transaction_type, transaction_details)
+      end
+    end
+  end
+end
